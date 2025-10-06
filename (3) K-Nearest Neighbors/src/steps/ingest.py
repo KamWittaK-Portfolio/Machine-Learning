f695742bdab23a8d6318064e425c965b0d482800
Kameron Fournillier <Kameron.r.fournillier@gmail.com>
2025-10-06 15:48:00 -0400

    k

--- (3) K-Nearest Neighbors/src/steps/ingest.py
+++ (3) K-Nearest Neighbors/src/steps/ingest.py
@@ -16,15 +16,15 @@ def load_data() -> pd.DataFrame:
     URL: str = os.getenv("URL", "")
     KEY: str = os.getenv("KEY", "")
 
+    if URL == "https://khlsxzricbhxlpokbdvh.supabase.co":
+        print("yes")
+
     # Create Supabase client
     supabase: Client = create_client(URL, KEY)
 
     # Query Supabase table
     response: Any = supabase.table("KNNAlgorithmDataset1").select("*").execute()
     df_supabase = pd.DataFrame(response.data)
-
-    print(len(response.data))
-    print(response.data[:5])
 
     # Combine CSV and Supabase data
     return pd.concat([df_csv, df_supabase], ignore_index=True)
