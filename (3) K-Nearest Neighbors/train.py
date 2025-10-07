2a621eeb26ee6d7d6f5c001b18fd2ea875435980
Kameron Fournillier <Kameron.r.fournillier@gmail.com>
2025-10-07 01:53:29 -0400

    Fixed DB

--- (3) K-Nearest Neighbors/train.py
+++ (3) K-Nearest Neighbors/train.py
@@ -1,10 +1,4 @@
 from src.pipelines.train_pipeline import training_pipeline
 
-from src.steps.addKwarg import update_artifact_store_client_kwargs
-import os
-
-
-
 if __name__ == "__main__":
-    update_artifact_store_client_kwargs(new_client_kwargs={"endpoint_url": os.getenv("ARTIFACT_URL"), "region_name": "us-east-2",}, store_name="Supabase", store_id=None)
     training_pipeline()
