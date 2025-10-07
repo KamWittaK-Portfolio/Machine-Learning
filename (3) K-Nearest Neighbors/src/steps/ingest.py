9a517822bbd22a058347ac6490e53ee406cebdf0
Kameron Fournillier <Kameron.r.fournillier@gmail.com>
2025-10-06 22:43:54 -0400

    Added DB

--- (3) K-Nearest Neighbors/src/steps/ingest.py
+++ (3) K-Nearest Neighbors/src/steps/ingest.py
@@ -13,8 +13,8 @@ def load_data() -> pd.DataFrame:
     df_csv = pd.read_csv("data/KNNAlgorithmDataset.csv")
     
     # Load environment variables
-    URL: str = os.getenv("URL", "")
-    KEY: str = os.getenv("KEY", "")
+    URL: str = os.getenv("DB_URL", "")
+    KEY: str = os.getenv("DB_KEY", "")
 
     # Create Supabase client
     supabase: Client = create_client(URL, KEY)
