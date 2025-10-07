d2c6d11c7d73d0f26908f7e2b96106f92c47c2ef
Kameron Fournillier <Kameron.r.fournillier@gmail.com>
2025-10-07 01:30:37 -0400

    Fixed DB

--- (3) K-Nearest Neighbors/src/pipelines/train_pipeline.py
+++ (3) K-Nearest Neighbors/src/pipelines/train_pipeline.py
@@ -1,7 +1,12 @@
+import os
 from zenml.pipelines import pipeline
 from src.steps.ingest import load_data
+#from src.steps.addKwarg import update_artifact_store_client_kwargs
 
+
+
+
+# Step 2: Define the pipeline
 @pipeline
 def training_pipeline():
     data = load_data()
-    
